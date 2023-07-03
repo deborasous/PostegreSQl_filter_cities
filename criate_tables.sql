f9244905ee68cdfd902f7430ca94f759ceeae251
deborasous <deborapatriciadp27@gmail.com>
2023-07-02 22:35:17 -0300

    [M2S07] Ex 6 - Consulta de Dados com Agregação e Having

--- criate_tables.sql
+++ criate_tables.sql
@@ -23,15 +23,15 @@ CREATE TABLE
         continente VARCHAR(200)
     );
 
+/* INSERIR DADOS ESPANHA*/
 
-/* INSERIR DADOS*/
-INSERT INTO pais( id, nome_pais, continente)
-VALUES (1, 'Brasil', 'América do Sul'), (2, 'Argentina', 'América do Sul'), (3, 'Estados Unidos', 'América do Norte'), (4, 'República Dominicana', 'América Central'), (5, 'El Salvador', 'América Central'), (6, 'Chile', 'América do Sul');
-
+INSERT INTO
+    pais(id, nome_pais, continente)
+VALUES (7, 'Espanha', 'Europa');
 
 INSERT INTO
     estado (id, id_pais, nome_estado)
-VALUES (1, 1, 'São Paulo'), (2, 1, 'Rio de Janeiro'), (3, 2, 'Província de Buenos Aires'), (4, 3, 'California'), (5, 4, 'Distrito Nacional'), (6, 1, 'Maranhão'), (7, 5, 'San Salvador'), (8, 6, 'Santiago');
+VALUES (16, 7, 'Andaluzia');
 
 INSERT INTO
     cidade (
@@ -41,67 +41,22 @@ INSERT INTO
         capital,
         qtd_populacao
     )
-VALUES (
-        1,
-        'São Paulo',
-        1,
-        'True',
-        1234567
-    ), (
-        2,
-        'Rio de Janeiro',
-        1,
-        'False',
-        987654
-    ), (
-        3,
-        'Buenos Aires',
-        2,
-        'True',
-        2897365
-    ), (
-        4,
-        'San Francisco',
-        5,
-        'True',
-        883305
-    ), (
-        5,
-        'Santo Domingo',
-        4,
-        'True',
-        965040
-    ), (
-        6,
-        'São Luís',
-        6,
-        'False',
-        1108975
-    ), (
-        7,
-        'San Salvador',
-        7,
-        'True',
-        567698
-    ), (
-        8,
-        'Santiago',
-        8,
-        'True',
-        7224845
-    );
+VALUES (17, 'Toulouse', 8, 'False', 479553);
 
 
 /*SCRIPT SQL*/
 
-SELECT -- SELECIONA AS COLUNAS COM NOMES DE CIDADE, ESTADO E PAÍS
+SELECT
+    -- SELECIONA AS COLUNAS COM NOMES DE CIDADE, ESTADO E PAÍS
     p.nome_pais,
     e.nome_estado,
     c.nome_cidade
-FROM cidade c -- DEFINE cidade COMO TABELA PRINCIPAL DA CONSULTA
+FROM
+    cidade c -- DEFINE cidade COMO TABELA PRINCIPAL DA CONSULTA
     JOIN estado e ON c.id_estado = e.id -- REALIZA A JUNÇÃO ENTRE A TABELA ESTADO E CIDADE / PAIS E ESTADO
     JOIN pais p ON e.id_pais = p.id
-WHERE -- FILTRA CIDADES QUE SÃO CAPITAIS, COM POPULAÇÃO MAIOR QUE 500 MIL E COM NOMES SANTOS
+WHERE
+    -- FILTRA CIDADES QUE SÃO CAPITAIS, COM POPULAÇÃO MAIOR QUE 500 MIL E COM NOMES SANTOS
     c.capital = 'True'
     AND c.qtd_populacao > 500000
     AND (
@@ -119,3 +74,30 @@ ORDER BY
     p.nome_pais,
     e.nome_estado,
     c.nome_cidade;
+
+/*RETORNAR PAISES QUE TENHAM MAIS HABITANTES QUE A POPULAÇÃO TOTAL DAS CIDADES DA ESPANHA*/
+
+SELECT
+    p.nome_pais,
+    SUM(c.qtd_populacao) AS total_populacao
+FROM cidade c
+    JOIN estado e ON c.id_estado = e.id
+    JOIN pais p ON e.id_pais = p.id
+GROUP BY p, nome_pais
+HAVING SUM(c.qtd_populacao) > (
+        SELECT
+            SUM(qtd_populacao)
+        FROM cidade c
+            JOIN estado e ON c.id_estado = e.id
+            JOIN pais p ON e.id_pais = p.id
+        WHERE
+            p.nome_pais = 'Espanha'
+    )
+ORDER BY total_populacao DESC;
+
+SELECT p.nome_pais, SUM(c.qtd_populacao) AS total_populacao
+FROM Pais p
+JOIN Estado e ON p.id = e.id_pais
+JOIN Cidade c ON e.id = c.id_estado
+WHERE p.continente = 'Europa'
+GROUP BY p.nome_pais;
